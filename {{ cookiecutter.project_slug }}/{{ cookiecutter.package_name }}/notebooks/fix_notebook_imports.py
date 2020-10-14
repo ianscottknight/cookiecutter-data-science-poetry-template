17f29d606a4bab77dca0847d40a4f247f68015a4
Ian Scott Knight <isk@alumni.stanford.edu>
2020-10-14 00:37:09 -0700

    adds python script to notebooks dir that when imported by adjacent jupyter notebooks fixes the sys path to allow imports relative to the project root directory

---
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.package_name }}/notebooks/fix_notebook_imports.py
@@ -0,0 +1,14 @@
+'''
+import this file within a given jupyter notebook to allow imports from the project root dir instead of notebooks dir
+e.g., 
+Run ```import fix_notebook_imports```
+```from src import util``` is now possible 
+'''
+
+import os 
+import sys
+
+module_path = os.path.abspath(os.path.join('..'))
+if module_path not in sys.path:
+    sys.path.append(module_path)
+    
